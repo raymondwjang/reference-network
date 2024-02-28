9699f37276496084d56d73a419ac1bca64f4d95c
Raymond W. Chang <raymond.w.jang@gmail.com>
2024-02-28 15:30:05 -0800

    feat: initiate main file

---
+++ main.py
@@ -0,0 +1,16 @@
+from config import CONFIG
+
+from reference_network import (
+    Publication,
+    PublicationDatabase,
+    ReferenceGraph,
+    GraphVisualizer,
+)
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
