60fe2718c860e486ea11677ce0abcf6caa57851f
Raymond W. Chang <raymond.w.jang@gmail.com>
2024-02-27 17:03:55 -0800

    feat: publication type suggestions

--- reference_network/publication.py
+++ reference_network/publication.py
@@ -1,10 +1,23 @@
+from typing import List
+
+
 class Publication:
-    def __init__(self, title, authors, year, doi, references):
+    def __init__(
+        self,
+        title: str,
+        authors: List[str],
+        year: int,
+        doi: str,
+        references: List[str] = [],
+    ):
         self.title = title
         self.authors = authors
         self.year = year
         self.doi = doi
         self.references = references
 
-    def add_reference(self, reference):
+    def add_reference(self, reference: str):
         self.references.append(reference)
+
+    def add_references(self, references: List[str]):
+        self.references.extend(references)
