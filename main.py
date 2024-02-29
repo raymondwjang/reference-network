dc20ff245ce6da4af4461dad4f211f97a7fb76b5
Raymond W. Chang <raymond.w.jang@gmail.com>
2024-02-28 16:59:06 -0800

    feat: main checks with test data

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from config import CONFIG
 
 from reference_network import (
-    Publication,
-    PublicationDatabase,
     ReferenceGraph,
     GraphVisualizer,
 )
 
 
 def main():
-    pass
+
+    # fetcher = DataFetcher()
+    graph = ReferenceGraph()
+    graph.ingest_publication_database(filled_publication_database)
+
+    visualizer = GraphVisualizer(graph, use_interactivity=True, plotly_config=CONFIG)
+    visualizer.visualize()
 
 
 if __name__ == "__main__":
