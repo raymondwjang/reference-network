b2b47a22f46b3f8654c6ea9338b12925cea507fa
Raymond W. Chang <raymond.w.jang@gmail.com>
2024-02-29 12:24:11 -0800

    test: separate data_parser and implement crossref call

--- tests/test_data_fetcher.py
+++ tests/test_data_fetcher.py
@@ -7,40 +7,23 @@
 #     Methods:
 #         Methods to fetch data by DOI, title, author, etc.
 #         Methods to parse fetched data into Publication objects
-import pytest
-
-from reference_network import DataFetcher, Publication, PublicationDatabase
 
 
-TEST_DATA_PATH = "tests/data/my_zotero_library.csv"
-
-
-@pytest.fixture
-def data_fetcher():
-    return DataFetcher(
-        semantic_scholar_api_key="my_semantic_scholar_api_key",
-    )
-
-
-def test_data_fetcher_load_zotero_exported_file(data_fetcher):
-    zotero_data = data_fetcher.load_zotero_exported_file(filepath=TEST_DATA_PATH)
+def test_data_fetcher_load_csv_exported_file(csv_data_fetcher):
+    zotero_data = csv_data_fetcher.load_zotero_exported_file()
     assert zotero_data is not None
     assert len(zotero_data) == 31
     assert "Title" in zotero_data.columns
     assert "DOI" in zotero_data.columns
 
 
-def test_data_fetcher_transform_row_to_publication(data_fetcher):
-    zotero_data = data_fetcher.load_zotero_exported_file(filepath=TEST_DATA_PATH)
-    publication = data_fetcher.transform_row_to_publication(zotero_data.iloc[0])
-    assert isinstance(publication, Publication)
-
-
-def test_data_fetcher_transform_df_to_publication_database(data_fetcher):
-    zotero_data = data_fetcher.load_zotero_exported_file(filepath=TEST_DATA_PATH)
-    publication_database = data_fetcher.transform_df_to_publication_database(
-        zotero_data
+def test_data_fetcher_fetch_references_by_doi(csv_data_fetcher):
+    references = csv_data_fetcher.fetch_references_by_doi(
+        "10.1371/journal.pcbi.1009832"
     )
-    assert isinstance(publication_database, PublicationDatabase)
-    assert len(publication_database.publications) == 29
-    assert isinstance(publication_database.publications[0], Publication)
+    assert references is not None
+    assert len(references) == 60
+    assert "DOI" in references[0]
+    assert "article-title" in references[0]
+    assert "author" in references[0]
+    assert "year" in references[0]
---
+++ tests/test_data_parser.py
@@ -0,0 +1,36 @@
+import pytest
+from reference_network import Publication, PublicationDatabase, DataParser
+
+
+@pytest.fixture
+def data_parser():
+    return DataParser()
+
+
+def test_data_parser_transform_row_to_publication(csv_data_fetcher, data_parser):
+    zotero_data = csv_data_fetcher.load_zotero_exported_file()
+    publication = data_parser.transform_row_to_publication(zotero_data.iloc[0])
+    assert isinstance(publication, Publication)
+
+
+def test_data_parser_transform_df_to_publication_database(
+    csv_data_fetcher, data_parser
+):
+    zotero_data = csv_data_fetcher.load_zotero_exported_file()
+    publication_database = data_parser.transform_df_to_publication_database(zotero_data)
+    assert isinstance(publication_database, PublicationDatabase)
+    assert len(publication_database.publications) == 29
+    assert isinstance(publication_database.publications[0], Publication)
+
+
+def test_data_parser_crossref_data_into_publication(
+    sample_publication, csv_data_fetcher, data_parser
+):
+    references = csv_data_fetcher.fetch_references_by_doi(
+        "10.1371/journal.pcbi.1009832"
+    )
+    publication = data_parser.crossref_data_into_publication(
+        sample_publication, references
+    )
+    assert isinstance(publication, Publication)
+    assert len(publication.references) == 50
