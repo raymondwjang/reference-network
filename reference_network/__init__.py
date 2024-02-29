d9daeffa072ebd1132954f192cb8fc8b7495693a
Raymond W. Chang <raymond.w.jang@gmail.com>
2024-02-29 12:25:22 -0800

    feat: add datafetch/parser in init

--- reference_network/__init__.py
+++ reference_network/__init__.py
@@ -2,4 +2,5 @@ from .graph_visualizer import GraphVisualizer
 from .publication import Publication
 from .publication_database import PublicationDatabase
 from .reference_graph import ReferenceGraph
-from .data_fetcher import DataFetcher
+from .data_fetcher import CSVDataFetcher, ZoteroDataFetcher
+from .data_parser import DataParser
