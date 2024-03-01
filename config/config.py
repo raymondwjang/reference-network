b6fdb420b46620195818f73b05437e8c0a6c4f5b
Raymond W. Chang <raymond.w.jang@gmail.com>
2024-02-29 16:15:40 -0800

    feat: cleanup config

--- config/config.py
+++ config/config.py
@@ -6,7 +6,6 @@ class DataConfig(BaseModel):
     data_path: str
     zotero_api_key: str
     library_id: str
-    semantic_scholar_api_key: str
 
     @classmethod
     def from_yaml(cls, file_path: str):
@@ -23,24 +22,11 @@ DATA_CONFIG = DataConfig.from_yaml("config/data_config.yaml")
 
 
 class PlotConfig(BaseModel):
-    edge_line: dict = Field(default_factory=lambda: {"width": 0.5, "color": "#888"})
-    node_marker: dict = Field(
-        default_factory=lambda: {
-            "showscale": True,
-            "colorscale": "YlGnBu",
-            "color": [],
-            "size": 10,
-        }
-    )
-    layout: dict = Field(
-        default_factory=lambda: {
-            "showlegend": False,
-            "hovermode": "closest",
-            "margin": {"b": 0, "l": 0, "r": 0, "t": 0},
-        }
-    )
+    base_size: int = 10
+    scaling_factor: int = 2
 
-    image_path: str = "output/reference_graph.gv"
+    interactive_graph_path: str = "figures/interactive_graph.html"
+    static_graph_path: str = "figures/reference_graph.gv"
 
     @classmethod
     def from_yaml(cls, file_path: str):
