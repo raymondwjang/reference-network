35deec1e60642a9182486c0a17d8c911f6c9c78f
Raymond W. Chang <raymond.w.jang@gmail.com>
2024-03-04 23:38:44 -0800

    chore: flake8 linting

--- reference_network/__init__.py
+++ reference_network/__init__.py
@@ -1,7 +1,7 @@
 from .data_fetcher import CSVDataFetcher, ZoteroDataFetcher
 from .data_parser import DataParser
 from .graph_visualizer import InteractiveVisualizer, StaticVisualizer
+from .reference_graph import ReferenceGraph
 
 # from .publication import Publication
 # from .publication_database import PublicationDatabase
-from .reference_graph import ReferenceGraph
