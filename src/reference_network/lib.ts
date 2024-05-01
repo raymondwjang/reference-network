9a0b6ca926436556e78e3c7c926aff3bd68abded
Raymond W. Chang <raymond.w.jang@gmail.com>
2024-04-30 18:16:20 -0700

    TODO: not working suddenly...

--- src/reference_network/lib.ts
+++ src/reference_network/lib.ts
@@ -1,39 +1,49 @@
-declare const Zotero: any
-declare const Components: any
+// Make sure the file is treated as a module
+export {};
+
+// Declare global variables
+declare global {
+  var Zotero: any;
+}
+
 const {
   // classes: Cc,
   // interfaces: Ci,
   utils: Cu,
-} = Components
+} = Components;
 
 if (Zotero.platformMajorVersion < 102) {
-  Cu.importGlobalProperties(['URL'])
+  Cu.importGlobalProperties(["URL"]);
 }
 
-Zotero.ReferenceNetwork = new class {
+Zotero.ReferenceNetwork = new (class {
   log(msg) {
-    Zotero.debug(`Reference Network: ${  msg}`)
+    Zotero.debug(`Reference Network: ${msg}`);
   }
 
   foo() {
     // Global properties are imported above in Zotero 6 and included automatically in
     // Zotero 7
-    const host = new URL('https://foo.com/path').host
-    this.log(`Host is ${host}`)
+    const host = new URL("https://foo.com/path").host;
+    this.log(`Host is ${host}`);
 
-    this.log(`Intensity is ${Zotero.Prefs.get('extensions.reference-network.intensity', true)}`)
+    this.log(
+      `Intensity is ${Zotero.Prefs.get(
+        "extensions.reference-network.intensity",
+        true
+      )}`
+    );
 
-    this.log(Zotero.getMainWindow().speechSynthesis)
+    this.log(Zotero.getMainWindow().speechSynthesis);
   }
 
   toggleGreen(enabled) {
-    const docElem = Zotero.getMainWindow().document.documentElement
+    const docElem = Zotero.getMainWindow().document.documentElement;
     // Element#toggleAttribute() is not supported in Zotero 6
     if (enabled) {
-      docElem.setAttribute('data-green-instead', 'true')
-    }
-    else {
-      docElem.removeAttribute('data-green-instead')
+      docElem.setAttribute("data-green-instead", "true");
+    } else {
+      docElem.removeAttribute("data-green-instead");
     }
   }
-}
+})();
