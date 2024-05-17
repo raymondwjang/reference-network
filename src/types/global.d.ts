4186455b859bf1705331497c0a25368fcb3d8860
Raymond W. Chang <raymond.w.jang@gmail.com>
2024-05-17 16:24:13 -0700

    feat: learned what d.ts is for (extending types from external libraries, basically)

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,25 +1,8 @@
 import "zotero-types";
 
 declare const Zotero: _ZoteroTypes.Zotero;
-declare var ReferenceNetwork: {
-  id: string | null;
-  version: string | null;
-  rootURI: string | null;
-  initialized: boolean;
-  addedElementIDs: string[];
-
-  init({
-    id,
-    version,
-    rootURI,
-  }: {
-    id: string;
-    version: string;
-    rootURI: string;
-  }): void;
-  log(msg: string): void;
-  toggleGreen(enabled: boolean): void;
-  main(): Promise<void>;
-};
-
-export {};
+declare global {
+  interface Window {
+    ZoteroPane: _ZoteroTypes.ZoteroPane;
+  }
+}
