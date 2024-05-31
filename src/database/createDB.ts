b57bb7df408ef56fd069f7ac8cfa63481e499ef8
Raymond W. Chang <raymond.w.jang@gmail.com>
2024-05-31 14:33:05 -0700

    TODO: How to initialize a db without bombing the bootstrap????

--- src/database/createDB.ts
+++ src/database/createDB.ts
@@ -1,26 +1,20 @@
-import { AuthorItemLink } from "./entity/authorItem";
+// import { AuthorItemLink } from "./entity/authorItem";
 import { Graph } from "./entity/graphs";
-import { Item } from "./entity/Items";
-import { Author } from "./entity/authors";
+// import { Item } from "./entity/Items";
+// import { Author } from "./entity/authors";
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 
-export const AppDataSource = new DataSource({
-  type: "sqlite",
-  database: "database.sqlite",
-  synchronize: true,
-  logging: false,
-  entities: [Item, Author, AuthorItemLink, Graph],
-  migrations: [],
-  subscribers: [],
-});
+Zotero.log("Reference Network (createDB.ts): Creating database...");
 
-AppDataSource.initialize()
-  .then(() => {
-    console.log("Connected to the database!");
-
-    // Add additional setup or testing code here
-  })
-  .catch((error) =>
-    console.log("Error during Data Source initialization:", error)
-  );
+export const AppDataSource = null;
+// export const AppDataSource = new DataSource({
+//   type: "sqlite",
+//   database: "database.sqlite",
+//   synchronize: true,
+//   logging: false,
+//   entities: [Graph],
+//   // entities: [Item, Author, AuthorItemLink, Graph],
+//   migrations: [],
+//   subscribers: [],
+// });
--- src/reference-network.ts
+++ src/reference-network.ts
@@ -1,6 +1,8 @@
 import { Shim } from "./environment/os";
 import { is7 } from "./environment/client";
-import { AppDataSource } from "./database/createDB";
+// import { AppDataSource } from "./database/createDB";
+import { DataSource } from "typeorm";
+import { Graph } from "./database/entity/graphs";
 
 const $OS = is7 ? Shim : OS;
 
@@ -40,8 +42,29 @@ export const ReferenceNetwork = {
     $OS.File.makeDir(this.dir, { ignoreExisting: true });
     this.log(`Directory created at ${this.dir}`);
 
-    // Attach New Database
-    this.db = AppDataSource;
+    // Attach New Database: This is the problem
+    // ok, so I can't even load it.
+    this.log(`Creating database...`);
+    const AppDataSource = new DataSource({
+      type: "sqlite",
+      database: "database.sqlite",
+      synchronize: true,
+      logging: false,
+      entities: [],
+      migrations: [],
+      subscribers: [],
+    });
+    // this.log(`Database created`);
+
+    // AppDataSource.initialize()
+    //   .then(() => {
+    //     console.log("Connected to the database!");
+
+    //     // Add additional setup or testing code here
+    //   })
+    //   .catch((error) =>
+    //     console.log("Error during Data Source initialization:", error)
+    //   );
     this.log("Database attached");
 
     this.initialized = true;
@@ -53,10 +76,7 @@ export const ReferenceNetwork = {
     this.log(`Host is ${host}`);
 
     this.log(
-      `Intensity is ${Zotero.Prefs.get(
-        "extensions.make-it-red.intensity",
-        true
-      )}`
+      `reference-network.ts: main() called with ${this.id}, ${this.version}, ${this.rootURI}`
     );
   },
 };
