e64cb74f4e843de4c865f627681acd7c03baad98
Raymond W. Chang <raymond.w.jang@gmail.com>
2024-06-14 19:01:08 -0700

    feat: condense data model

--- src/database/entities.ts
+++ src/database/entities.ts
@@ -5,18 +5,15 @@ export const DATABASE_NAMES = {
 export const TABLE_NAMES = {
   GRAPHS: `graphs`,
   ITEMS: `items`,
-  REFERENCED_WORKS: `referenced_works`,
-  RELATED_WORKS: `related_works`,
-  CITED_BY: `cited_by`,
 };
 
 export const DDL_QUERIES = {
   [TABLE_NAMES.GRAPHS]: `
     CREATE TABLE IF NOT EXISTS ${DATABASE_NAMES.WEAVER}.${TABLE_NAMES.GRAPHS} (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
       source TEXT,
-      type TEXT,
       target TEXT,
+      type TEXT check(type = "related_to" or type = "cites"),
       data_source TEXT,
       created_at TEXT DEFAULT CURRENT_TIMESTAMP,
       updated_at TEXT DEFAULT CURRENT_TIMESTAMP
@@ -24,38 +21,10 @@ export const DDL_QUERIES = {
   `,
   [TABLE_NAMES.ITEMS]: `
     CREATE TABLE IF NOT EXISTS ${DATABASE_NAMES.WEAVER}.${TABLE_NAMES.ITEMS} (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
       zotero_item_id TEXT,
       openalex_id TEXT,
       doi TEXT,
-      created_at TEXT DEFAULT CURRENT_TIMESTAMP,
-      updated_at TEXT DEFAULT CURRENT_TIMESTAMP
-    );
-  `,
-  [TABLE_NAMES.REFERENCED_WORKS]: `
-    CREATE TABLE IF NOT EXISTS ${DATABASE_NAMES.WEAVER}.${TABLE_NAMES.REFERENCED_WORKS} (
-      id TEXT PRIMARY KEY,
-      item_id TEXT,
-      cites TEXT,
-      created_at TEXT DEFAULT CURRENT_TIMESTAMP,
-      updated_at TEXT DEFAULT CURRENT_TIMESTAMP
-    );
-  `,
-  [TABLE_NAMES.RELATED_WORKS]: `
-    CREATE TABLE IF NOT EXISTS ${DATABASE_NAMES.WEAVER}.${TABLE_NAMES.RELATED_WORKS} (
-      id TEXT PRIMARY KEY,
-      item_id TEXT,
-      is_related_to TEXT,
-      created_at TEXT DEFAULT CURRENT_TIMESTAMP,
-      updated_at TEXT DEFAULT CURRENT_TIMESTAMP
-    );
-  `,
-  [TABLE_NAMES.CITED_BY]: `
-    CREATE TABLE IF NOT EXISTS ${DATABASE_NAMES.WEAVER}.${TABLE_NAMES.CITED_BY} (
-      id TEXT PRIMARY KEY,
-      item_id TEXT,
-      cited_by TEXT,
-      citer_in_library BOOLEAN,
       created_at TEXT DEFAULT CURRENT_TIMESTAMP,
       updated_at TEXT DEFAULT CURRENT_TIMESTAMP
     );
