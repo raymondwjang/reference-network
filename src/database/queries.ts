3180b590a339ecaeaa0537bb968ebdff1e18b44c
Raymond W. Chang <raymond.w.jang@gmail.com>
2024-06-14 19:01:24 -0700

    feat: parametrize library id in query

--- src/database/queries.ts
+++ src/database/queries.ts
@@ -1,11 +1,11 @@
 export const queries = {
-  getDOIs: `
+  getDOIs: (libraryID: number) => `
           SELECT value -- iD.itemID, iDV.valueID, fieldName
           FROM itemDataValues AS iDV
                    LEFT JOIN itemData AS iD ON iDV.valueID = iD.valueID
                    LEFT JOIN fields AS f ON iD.fieldID = f.fieldID
                    LEFT JOIN items AS i ON iD.itemID = i.itemID
           WHERE fieldName = 'DOI'
-            AND libraryID = 1;
+            AND libraryID = ${libraryID};
       `,
 };
