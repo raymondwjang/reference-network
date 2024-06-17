5b965633e499ed9b8231fd4c94b737d44433a927
Raymond W. Chang <raymond.w.jang@gmail.com>
2024-06-17 16:21:03 -0700

    ARE YOU FUCKING KIDDING ME

--- src/database/queries.ts
+++ src/database/queries.ts
@@ -1,11 +1,11 @@
 export const queries = {
-  getDOIs: (libraryID: number) => `
-          SELECT value as doi, iD.itemID -- iDV.valueID, fieldName
+  getDOIs: (
+    libraryID: number
+  ) => `SELECT value as doi, iD.itemID -- iDV.valueID, fieldName
           FROM itemDataValues AS iDV
                    LEFT JOIN itemData AS iD ON iDV.valueID = iD.valueID
                    LEFT JOIN fields AS f ON iD.fieldID = f.fieldID
                    LEFT JOIN items AS i ON iD.itemID = i.itemID
           WHERE fieldName = 'DOI'
-            AND libraryID = ${libraryID};
-      `,
+            AND libraryID = ${libraryID};`,
 };
